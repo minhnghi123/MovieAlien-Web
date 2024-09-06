4a029b6ccf7aece4d38c4523b126d6cb7349a9c2
minhnghi123 <minhhnghiii@gmail.com>
2024-09-06 22:59:28 +0700

    completed homePage Pug

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT;
+//view engine configuration
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+//middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home/pages/index");
+});
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
