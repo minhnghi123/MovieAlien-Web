ae4fee01b6334a9aeb83a5b42b2469de13ed5a0a
Thais Mai <thai2k4>
2024-09-10 09:22:05 +0700

    pug page conversion

--- server.js
+++ server.js
@@ -19,6 +19,34 @@ app.get("/batman", (req, res) => {
   res.render("home/pages/batman.pug");
 });
 
+app.get("/eternals", (req, res) => {
+  res.render("home/pages/eternals.pug");
+});
+
+app.get("/jumanji", (req, res) => {
+  res.render("home/pages/jumanji.pug");
+});
+
+app.get("/junglecruise", (req, res) => {
+  res.render("home/pages/junglecruise.pug");
+});
+
+app.get("/shangchi", (req, res) => {
+  res.render("home/pages/shangchi.pug");
+});
+
+app.get("/uncharted", (req, res) => {
+  res.render("home/pages/uncharted.pug");
+});
+
+app.get("/wolverine", (req, res) => {
+  res.render("home/pages/wolverine.pug");
+});
+
+app.get("/flash", (req, res) => {
+  res.render("home/pages/flash.pug");
+});
+
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
